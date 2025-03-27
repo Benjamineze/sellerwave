0c3bd53c0be5f9a8977c758e484e87bb2281f595
Benjamin Eze <62108933+Benjamineze@users.noreply.github.com>
2025-03-27 22:55:07 +0000

    Update stories.py

--- stories.py
+++ stories.py
@@ -101,7 +101,7 @@ def show_stories(df):
     result["Qty Sold"] = result["Qty Sold"].apply(lambda x: f"{x:,}")
     result["Price"] = result["Price"].apply(lambda x: f"{x:,.2f}")
     # Wrap the 'Product Name' text to a maximum width (e.g., 30 characters per line)
-    result['Product Name'] = result['Product Name'].apply(lambda x: '\n'.join(textwrap.wrap(x, width=30)))
+    result['Product Name'] = result['Product Name'].apply(lambda x: '\n'.join(textwrap.wrap(x, width=10)))
    # Reorder columns for better readability
     result = result[['Product Name', 'Price', 'Qty Sold']]
     
