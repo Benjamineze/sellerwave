e956718ecf544d4425c0b3032a613e2149257910
Benjamin Eze <62108933+Benjamineze@users.noreply.github.com>
2025-03-27 21:04:40 +0000

    Update Amazon_dashboard.py

--- Amazon_dashboard.py
+++ Amazon_dashboard.py
@@ -13,6 +13,7 @@ import os
 from google.oauth2 import service_account
 from google.auth import default
 import toml
+import textwrap
 
 
 #Load credentials from TOML file
@@ -20,7 +21,7 @@ credentials_info = st.secrets["GOOGLE_CREDENTIAL_FOR_AMAZON"]
 
 credentials = service_account.Credentials.from_service_account_info(credentials_info)
 
-# Initialize BigQuery Client with correct credentials
+# Initialize BigQuery Client with  credentials
 client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
 
@@ -59,6 +60,7 @@ def prepare_data(df):
 
 
 
+
 def show_Amazon_dashboard(df):
     # Streamlit app layout
 
@@ -79,11 +81,13 @@ def show_Amazon_dashboard(df):
     percentages = counts / total_count * 100
 
     # Create the plot
-    plt.figure(figsize=(8, 4))
+    plt.figure(figsize=(8, 5))
     ax = sns.countplot(x='Product Category', data=df, order=counts.index, color='skyblue')
     plt.xlabel('Product Category', labelpad=20)
     plt.ylabel("Number of Products")
     plt.title('Individual Distribution of Product Categories', pad=15)
+    wrapped_labels = [textwrap.fill(label, width=10) for label in df['Product Category'].unique()]
+    ax.set_xticklabels(wrapped_labels, rotation=0)  # Keep them horizontal
 
     # Adjusting space above the highest bar
     plt.ylim(0, max(counts) * 1.2)
@@ -112,17 +116,21 @@ def show_Amazon_dashboard(df):
     category_sales = df.groupby('Product Category')['Qty Sold'].sum().reset_index()
     total_qty_sold = category_sales['Qty Sold'].sum()
     category_sales['Percentage'] = (category_sales['Qty Sold'] / total_qty_sold) * 100
+    
+    
 
     category_sales = category_sales.sort_values(by='Qty Sold', ascending=False)
 
     # Create the plot
-    plt.figure(figsize=(9, 6))
+    plt.figure(figsize=(8, 5))
     ax = sns.barplot(x='Product Category', y='Qty Sold', data=category_sales, palette='viridis', hue='Product Category', dodge=False)
 
-    plt.xlabel("Product Category", labelpad=20)
+    plt.xlabel("Product Category", labelpad=20, )
     plt.ylabel("Total Quantity Sold")
     plt.title("Total Quantity Sold by Product Category", pad=15)
-
+    # Wrap the text labels
+    wrapped_labels = [textwrap.fill(label, width=10) for label in category_sales['Product Category'].unique()]
+    ax.set_xticklabels(wrapped_labels, rotation=0)  # Keep them horizontal
     # Adding more space above the highest bar for better readability
     plt.ylim(0, max(category_sales['Qty Sold']) * 1.2)
 
@@ -197,12 +205,13 @@ def show_Amazon_dashboard(df):
 
 
     # Calculate total quantity sold by category and percentages
+
     category_sales = df.groupby('Rating_cat')['Qty Sold'].sum().reset_index()
     total_qty_sold = category_sales['Qty Sold'].sum()
     category_sales['Percentage'] = (category_sales['Qty Sold'] / total_qty_sold) * 100
 
     # Create the plot
-    plt.figure(figsize=(6, 5))
+    plt.figure(figsize=(9, 6))
     ax = sns.barplot(x='Rating_cat', y='Qty Sold', data=category_sales, palette='viridis', hue='Rating_cat', dodge=False)
 
     plt.xlabel("Rating Category", labelpad=20)
@@ -278,6 +287,10 @@ def show_Amazon_dashboard(df):
 
 
     # TOP 10 POTENTIAL HIGH SALE PRODUCTS
+    st.markdown(
+    "<h1 style='color:grey; font-size: 17px; font-weight: bold; font-style: italic;'>Top 10 </span><span style='color:green;'>Potential High sale products</h1>", 
+    unsafe_allow_html=True
+    )
 
     def calculate_moving_average(product_df, window=3):
         """Calculate the moving average of sales for each product."""
@@ -345,18 +358,17 @@ def show_Amazon_dashboard(df):
 
 
     st.markdown(
-    "<h1 style='color:grey; font-size: 18px; font-weight: bold; font-style: italic;'> Products with <span style='color:green;'>Month-on-Month Growth <span style='color:blue;'>( 3 months)</h1>", 
+    "<h1 style='color:grey; font-size: 17px; font-weight: bold; font-style: italic;'>Products with </span><span style='color:blue;'>Month-on-Month growth (3 Months)</h1>", 
     unsafe_allow_html=True
     )
-    
+
     # Prepare the data
     df, third_last_month, last_month, current_month, last_three_months = prepare_data(df)
 
-    # Check if at least last two months are available
-    if len(last_three_months) < 2:
-        st.write("Last 2 months data are not available.")
+     # If only 1 month exists, display a message and return
+    if third_last_month is None and last_month == current_month:
+        st.write("Not enough data to calculate growth.")
         return
-
 
     # Filter products sold in the last three months
     products_sold_last_months = df[df['Month'].isin(last_three_months)]
@@ -376,76 +388,87 @@ def show_Amazon_dashboard(df):
     # Pivot the data to get separate columns for each month's quantity sold
     pivot_result = result.pivot(index='Product Name', columns='Month', values='Qty Sold').reset_index()
 
-    # Filter the data for positive growth in each of the last three months
-    pivot_result = pivot_result[
+    # Calculate growth only if enough data is available
+    if third_last_month in pivot_result.columns:
+        # Filter the data for positive growth in each of the last three months
+        pivot_result = pivot_result[
         (pivot_result[current_month] > pivot_result[last_month]) &
         (pivot_result[last_month] > pivot_result[third_last_month])
-    ]
-
-    # Prepare data for the plot
-    plot_data = result[result['Product Name'].isin(pivot_result['Product Name'])]
-
-    # Sort the category sales DataFrame in descending order of Qty Sold
-    plot_data = plot_data.sort_values(by='Qty Sold', ascending=True)
+        ]
+    
 
 
-    plot_data['Product Name'] =plot_data['Product Name'].apply(lambda x: '\n'.join(textwrap.wrap(x, 60)))
+        # Prepare data for the plot
+        plot_data = result[result['Product Name'].isin(pivot_result['Product Name'])]
+
+        # Sort the category sales DataFrame in descending order of Qty Sold
+        plot_data = plot_data.sort_values(by='Qty Sold', ascending=True)
 
 
-    # Plotting the month-on-month growth
-    fig, ax = plt.subplots(figsize=(10, 8))
-    sns.barplot(data=plot_data, x='Qty Sold', y='Product Name', hue='Month', ax=ax)
-    ax.set_title("Month-on-Month Positive Growth (3months)", pad=15)
-    ax.set_xlabel("Month", labelpad=20)
-    ax.set_ylabel("Total Quantity Sold")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    # Rotate x-axis labels for better readability and adjust size
-    plt.xticks(rotation=45, ha="right", fontsize=12)
+        plot_data['Product Name'] =plot_data['Product Name'].apply(lambda x: '\n'.join(textwrap.wrap(x, 60)))
 
 
-    # Adding more space above the highest bar for better readability
-    max_value = plot_data['Qty Sold'].max()  # Find the maximum value in the data
-    plt.xlim(0, max_value * 1.2)  # Adjust x-axis limit to add extra space above the highest bar
+        # Plotting the month-on-month growth
+        fig, ax = plt.subplots(figsize=(10, 8))
+        sns.barplot(data=plot_data, x='Qty Sold', y='Product Name', hue='Month', ax=ax)
+        ax.set_title("Month-on-Month Positive Growth (3months)", pad=15)
+        ax.set_xlabel("Month", labelpad=20)
+        ax.set_ylabel("Total Quantity Sold")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        # Rotate x-axis labels for better readability and adjust size
+        plt.xticks(rotation=45, ha="right", fontsize=12)
 
-    # Annotate bars with values (the quantity sold)
-    for p in ax.patches:
-        width = p.get_width()
-        if width < 0:  # Avoid negative values if present
-            width = 0
+
+        # Adding more space above the highest bar for better readability
+        max_value = plot_data['Qty Sold'].max()  # Find the maximum value in the data
+        plt.xlim(0, max_value * 1.2)  # Adjust x-axis limit to add extra space above the highest bar
+
+        # Annotate bars with values (the quantity sold)
+        for p in ax.patches:
+            width = p.get_width()
+            if width < 0:  # Avoid negative values if present
+                width = 0
+            
+            # Adjust annotation position: Ensure it stays inside the bars
+            ax.annotate(f'{int(width):,}', 
+                        (width + 5, p.get_y() + p.get_height() / 2),  # Position slightly inside the bar
+                        ha='left', va='center', color='black', fontsize=10)
+
+        # Adjust y-axis labels' font size
+        ax.tick_params(axis='y', labelsize=12)
+        # Display the plot in Streamlit
+        st.pyplot(fig)
+    else:
+        st.write("<h1 style='color:#BD7E58; font-size: 20px; font-weight: bold; font-style: italic;'>ooopss.....Not enough data to calculate growth.</h1>", 
+        unsafe_allow_html=True)
         
-        # Adjust annotation position: Ensure it stays inside the bars
-        ax.annotate(f'{int(width):,}', 
-                    (width + 5, p.get_y() + p.get_height() / 2),  # Position slightly inside the bar
-                    ha='left', va='center', color='black', fontsize=10)
-
-    # Adjust y-axis labels' font size
-    ax.tick_params(axis='y', labelsize=12)
-    # Display the plot in Streamlit
-    st.pyplot(fig)
 
 
 
-
-#CURRENT VS LAST MONTH GROWTH (2 MONTH)
-    st.write("#### Product with Month-on-Month Growth (2 Months)")
-
+    #CURRENT VS LAST MONTH GROWTH (2 MONTH)
+    
     st.markdown(
-    "<h1 style='color:grey; font-size: 18px; font-weight: bold; font-style: italic;'> Products with <span style='color:green;'>Month-on-Month Growth <span style='color:blue;'>( 2 months)</h1>", 
+    "<h1 style='color:grey; font-size: 17px; font-weight: bold; font-style: italic;'>Products with </span><span style='color:blue;'>Month-on-Month growth (2 Months)</h1>", 
     unsafe_allow_html=True
     )
-    
+        
     # Prepare the data (using the function)
     df, third_last_month, last_month, current_month, last_three_months = prepare_data(df)
 
-    # Filter products sold in the last three months
+        # If only 1 month exists, display a message and return
+    if third_last_month is None and last_month == current_month:
+            st.write("Not enough data to calculate growth.")
+            return
+
+
+        # Filter products sold in the last three months
     products_sold_last_months = df[df['Month'].isin(last_three_months)]
 
-    # Find common products sold across all three months (if available)
+     # Find common products sold across all three months (if available)
     common_products = set(products_sold_last_months[products_sold_last_months['Month'] == current_month]['Product Name']).intersection(
-        set(products_sold_last_months[products_sold_last_months['Month'] == last_month]['Product Name']),
-        set(products_sold_last_months[products_sold_last_months['Month'] == third_last_month]['Product Name'])
-    )
+            set(products_sold_last_months[products_sold_last_months['Month'] == last_month]['Product Name']) 
+        )
 
     # Filter the DataFrame for these common products
     common_products_df = products_sold_last_months[products_sold_last_months['Product Name'].isin(common_products)]
@@ -456,14 +479,16 @@ def show_Amazon_dashboard(df):
     # Pivot the data to get separate columns for each month's quantity sold
     pivot_result = result.pivot(index='Product Name', columns='Month', values='Qty Sold').reset_index()
 
+
+        
     # Filter the data to only include rows where:
-    # current month sales > last month sales AND last month sales > third last month sales
+    # current month sales > last month sales 
     pivot_result = pivot_result[
-        (pivot_result[current_month] > pivot_result[last_month]) 
-    ]
-    
-    
-    
+            (pivot_result[current_month] > pivot_result[last_month]) 
+        ]
+        
+        
+        
     pivot_result = result.pivot(index='Product Name', columns='Month', values='Qty Sold').reset_index()
 
     # Filter for positive growth
@@ -471,15 +496,14 @@ def show_Amazon_dashboard(df):
 
     # Melt the pivot result to long format for plotting
     melted_data = pivot_result.melt(id_vars=["Product Name"], value_vars=[last_month, current_month],
-                                    var_name="Month", value_name="Qty Sold")
-    
-    
-    
-    
+                                        var_name="Month", value_name="Qty Sold")
+        
+        
+
 
     # Plotting
     fig, ax = plt.subplots(figsize=(14, 12))  
-    
+        
 
     # Wrap long product names for readability
     melted_data['Product Name'] = melted_data['Product Name'].apply(lambda x: '\n'.join(textwrap.wrap(x, 60)))
@@ -493,14 +517,14 @@ def show_Amazon_dashboard(df):
 
     # Annotate bars with values (the quantity sold)
     for p in ax.patches:
-        width = p.get_width()
-        if width < 0:  # Avoid negative values if present
-            width = 0
-        
-        # Adjust annotation position: Ensure it stays inside the bars
-        ax.annotate(f'{int(width):,}', 
-                    (width + 5, p.get_y() + p.get_height() / 2),  # Position slightly inside the bar
-                    ha='left', va='center', color='black', fontsize=12)
+            width = p.get_width()
+            if width < 0:  # Avoid negative values if present
+                width = 0
+            
+            # Adjust annotation position: Ensure it stays inside the bars
+            ax.annotate(f'{int(width):,}', 
+                        (width + 5, p.get_y() + p.get_height() / 2),  # Position slightly inside the bar
+                        ha='left', va='center', color='black', fontsize=12)
 
     # Set titles and labels with larger font size
     ax.set_title("+ve GROWTH for Current vs Last Month (2 Months)", fontsize=16,  pad=15)
@@ -508,20 +532,18 @@ def show_Amazon_dashboard(df):
     ax.set_ylabel("Product Name", fontsize=14)
 
 
-   
+    
     # Rotate x-axis labels for better readability and adjust size
     plt.xticks(rotation=45, ha="right", fontsize=12)
 
     # Adjust y-axis labels' font size
     ax.tick_params(axis='y', labelsize=12)
 
-     # Adjust layout to prevent clipping of labels
+    # Adjust layout to prevent clipping of labels
     plt.tight_layout()
 
     # Display the plot in the Streamlit app
     st.pyplot(fig)
-
-
 
 
 
